fb5c99dfb60c11987c1381b749aa546a80c45b9c
Gh0st-z <shinigamikami446@gmail.com>
2024-04-21 10:46:36 +0545

    added home, user and product pages

--- epharmacy_backend/urls.py
+++ epharmacy_backend/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import re
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path as url, include
+from django.views.static import serve
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -27,5 +29,12 @@ urlpatterns = [
     path('order/', include('order.urls')),
     path('invoice/', include('invoice.urls')),
     path('features/', include('features.urls')),
+    path('home/', include('home.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+static_media_url = [
+    url(r'^%s(?P<path>.*)$' % re.escape(settings.MEDIA_URL.lstrip('/')), serve, kwargs={'document_root': settings.MEDIA_ROOT}),
+    url(r'^%s(?P<path>.*)$' % re.escape(settings.STATIC_URL.lstrip('/')), serve, kwargs={'document_root': settings.STATIC_ROOT})
+]
+
+urlpatterns += static_media_url
